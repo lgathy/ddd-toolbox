842345a9d726da13d0c7b995e37b577a6f9cb019
Gathy Lajos <gathy.lajos@gmail.com>
2017-06-15 04:47:52 +0200

    Added: PagedList.transform(mapperFun)

--- ddd-api/src/main/java/com/doctusoft/ddd/model/PagedList.java
+++ ddd-api/src/main/java/com/doctusoft/ddd/model/PagedList.java
@@ -5,6 +5,10 @@ import lombok.Data;
 
 import java.io.Serializable;
 import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+import static java.util.Objects.requireNonNull;
 
 @Data
 @AllArgsConstructor
@@ -15,5 +19,10 @@ public final class PagedList<T> implements PagedResponse<T>, Serializable {
     private List<T> pageRows;
     
     private int totalRowCount;
+    
+    public <U> PagedList<U> transform(Function<? super T, ? extends U> mapperFun) {
+        requireNonNull(mapperFun, "mapperFun");
+        return new PagedList<>(pageRows.stream().map(mapperFun).collect(Collectors.toList()), totalRowCount);
+    }
     
 }
